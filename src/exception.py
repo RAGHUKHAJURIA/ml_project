7ed08ff751692700ab6fb9e7532a66ca5c721581
RAGHUKHAJURIA <khajuriaraghu0@gmail.com>
2025-11-23 13:36:21 +0530

    exception and logging

---
+++ src/exception.py
@@ -0,0 +1,21 @@
+import sys
+
+
+def error_message_detail(error, error_details:sys):
+    _,_,exc_tb = error_details.exc_info()
+    fileName = exc_tb.tb_frame.f_code.co_filename
+    error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".fromat(
+        fileName, exc_tb.tb_lineno, str(error)
+        
+    )
+    return error_message
+    
+
+
+class CustomException(Exception):
+    def __init__(self, erro_message, error_details:sys):
+        super().__init__(erro_message)
+        self.error_message = error_message_detail(erro_message, error_details=error_details)
+
+    def __str__(self):
+        return self.error_message
